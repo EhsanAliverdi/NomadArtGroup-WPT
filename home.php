4e88f11f48d10ca62bd237f0c68becf05d5ea478
ealiverdi <aliverdi.ehsan@hutchisonports.com.au>
2023-08-01 09:20:15 +1000

    fixed Home page with fixed text justified content center

--- home.php
+++ home.php
@@ -2,18 +2,22 @@
 /*
 Template Name: Custom Homepage
 */
-
-get_header();
 ?>
-
-<!-- Custom homepage content goes here -->
-<section id="custom-homepage-section" class="d-flex justify-content-center text-center">
-    <div class="d-flex justify-content-center text-center">
-        <h1>Welcome to Nomad Art Group!</h1>
-        <p>This is the custom homepage content.</p>
+<div class="container-fluid h-100">
+    <div class="row justify-content-center h-100">
+        <div class="col-12">
+            <div class="h-100 d-flex flex-column">
+                <?php  get_header(); ?>
+                <div class="row justify-content-center flex-grow-1">
+                    <div  class="d-flex align-items-center">
+                        <div class="align-self-center justify-content-center ps-5 pe-5">
+                            <h1 class="text-center">Welcome to Nomad Art Group!</h1>
+                            <p class="text-justify">Nomad Art     Group built on the strong traditions of presenting creative art performances to the audience in Australia. From our humble beginnings as a small and fledgeling community theatre, we have helped bring laughter, drama and the best voices from around the Country to our stages.</p>
+                        </div>
+                    </div>
+                </div>
+                <?php  get_footer(); ?>
+            </div>
+        </div>
     </div>
-</section>
-
-<?php
-get_footer();
-?>
+</div>
